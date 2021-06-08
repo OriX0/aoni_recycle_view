bf5db21674a268c695f9973b3aa3175048e06184
OriX0 <1098888910@qq.com>
2021-06-08 20:51:35 +0800

    🎉 feat: init project

---
+++ src/layouts/UserLayout.jsx
@@ -0,0 +1,59 @@
+import {  getMenuData, getPageTitle } from '@ant-design/pro-layout';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { Link, SelectLang, useIntl, connect } from 'umi';
+import React from 'react';
+import logo from '../../public/svg/recycle.svg';
+import styles from './UserLayout.less';
+
+const UserLayout = (props) => {
+  const {
+    route = {
+      routes: [],
+    },
+  } = props;
+  const { routes = [] } = route;
+  const {
+    children,
+    location = {
+      pathname: '',
+    },
+  } = props;
+  const { formatMessage } = useIntl();
+  const { breadcrumb } = getMenuData(routes);
+  const title = getPageTitle({
+    pathname: location.pathname,
+    formatMessage,
+    breadcrumb,
+    ...props,
+  });
+  return (
+    <HelmetProvider>
+      <Helmet>
+        <title>{title}</title>
+        <meta name="description" content={title} />
+      </Helmet>
+
+      <div className={styles.container}>
+        <div className={styles.lang}>
+          <SelectLang />
+        </div>
+        <div className={styles.content}>
+          <div className={styles.top}>
+            <div className={styles.header}>
+              <Link to="/">
+                <img alt="logo" className={styles.logo} src={logo} />
+                <span className={styles.title}>奥尼回收后台</span>
+              </Link>
+            </div>
+            <div className={styles.desc}>
+              一个用来回收奥尼摄像头的后台
+            </div>
+          </div>
+          {children}
+        </div>
+      </div>
+    </HelmetProvider>
+  );
+};
+
+export default connect(({ settings }) => ({ ...settings }))(UserLayout);
